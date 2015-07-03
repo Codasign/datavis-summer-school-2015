11892e9f1c03bfc7aa7e3519708836dc87dbb8f5
Becky Stewart <becky@codasign.com>
2015-07-03 14:46:57 +0000

    Changed from Tkinter to Pygame and implemented stop button

--- data-collection.py
+++ data-collection.py
@@ -1,3 +1,4 @@
+import pygame
 import gps
 import HTU21DF as htu
 
@@ -5,7 +6,24 @@ import HTU21DF as htu
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-while True:
+
+# read in and display temperature
+def read_temp():
+    t = htu.read_temperature()
+
+
+# set up window
+pygame.init()
+screen = pygame.display.set_mode((320, 150))
+done = False
+
+# variables for stop button
+stop_x = 10
+stop_y = 10
+stop_width = 100
+stop_height = 100
+
+def data_label(label):
     try:
     	report = session.next()
 	# Wait for a 'TPV' report and display the current time
@@ -28,3 +46,19 @@ while True:
 		session = None
 		print "GPSD has terminated"
 
+
+is_stop_pressed = False
+
+while not done:
+    for event in pygame.event.get():
+        # if window was closed, quit
+        if event.type == pygame.QUIT:
+            done = True
+        # if stop button was pressed, quite
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if (x>stop_x and x<stop_x+stop_width and y>stop_y and y<stop_y+stop_height): 
+                done = True
+
+    pygame.draw.rect(screen, (128, 12, 25), pygame.Rect(stop_x, stop_y, stop_width, stop_height))
+    pygame.display.flip()
